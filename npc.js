90fa920fb9f71429f62d104afd308d579b53efcb
Abhay <abhayk97@gmail.com>
2018-10-20 18:01:44 +0530

    v3

--- npc.js
+++ npc.js
@@ -1,7 +1,7 @@
 //dependencies
 let robot = require("robotjs");
 let Jimp = require("jimp");
-var screenshot = require('desktop-screenshot');
+let screenshot = require('desktop-screenshot');
 let xPos = 50;
 let yPos = -50;
 
@@ -24,10 +24,10 @@ let interval = setInterval(function(){
 			//to x = 232 + 900 and y = 161 + 400(focusing on the center of the screen)
 			//below function is executed for every pixel(x,y) on the image
 			image.scan(232, 161, 900, 400, function(x, y, idx) {
-				var red = this.bitmap.data[idx + 0];
-				var green = this.bitmap.data[idx + 1];
-				var blue = this.bitmap.data[idx + 2];
-				var alpha = this.bitmap.data[idx + 3];
+				let red = this.bitmap.data[idx + 0];
+				let green = this.bitmap.data[idx + 1];
+				let blue = this.bitmap.data[idx + 2];
+				let alpha = this.bitmap.data[idx + 3];
 				//condition to check for green color, if true, save the x and y
 				if(red<50 && green>200 && blue<50){
 					xPos = x;
